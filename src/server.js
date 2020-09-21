78808bbc417dd111bc933345af131861754cd015
Irving Espiritu <irving.espiritu@agilethought.com>
2020-09-21 17:51:25 -0500

    code fixed

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 // Settings
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname , 'views'));
-app.set('.hbs', exphbs({
+app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
