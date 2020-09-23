be323b3dcfe4c4366e5d238938ebbc068f5c0314
Luis Antonio Pérez Ponce <luis.perez@agilethought.com>
2020-09-23 12:14:48 -0500

    raen

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -1,29 +1,40 @@
 const notesCtrl = {};
 
+const Note = require('../models/Note');
+
 notesCtrl.renderNoteForm = (req, res) => {
   res.render('notes/new-note');
 };
 
-notesCtrl.createNewNote = (req, res) => {
-  console.log(req.body)
-    res.send("new note");
-  };
-
-  
-notesCtrl.renderNotes = (req, res) => {
-    res.send("render notes");
+notesCtrl.createNewNote = async (req, res) => {
+  const {title, description} = req.body;
+  const newNote = new Note({title, description});
+  await newNote.save();
+  req.flash('success_msg', 'Note Added Successfully')
+  res.redirect('/notes');
   };
   
-  notesCtrl.renderEditForm = (req, res) => {
-    res.send("render edit form");
+notesCtrl.renderNotes = async (req, res) => {
+    const notes = await Note.find();
+    res.render('notes/all-notes', {notes});
+  };
+   
+  notesCtrl.renderEditForm = async (req, res) => {
+    const note = await Note.findById(req.params.id);
+    res.render('notes/edit-note',{ note });
   };
   
-  notesCtrl.updateNote = (req, res) => {
-    res.send("update note");
+  notesCtrl.updateNote = async (req, res) => {
+    const { title, description } = req.body;
+    await Note.findByIdAndUpdate(req.params.id, { title, description})
+    req.flash('success_msg', 'Note Updated Successfully');
+    res.redirect('/notes');
   };
   
-  notesCtrl.deleteNote = (req, res) => {
-    res.send("deleting note");
+  notesCtrl.deleteNote = async (req, res) => {
+    await console.log(req.params.id);
+    req.flash('success_msg', 'Note Deleted Successfully');
+    res.redirect('/notes');
   };
 
 module.exports = notesCtrl;
